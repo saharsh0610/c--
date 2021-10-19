0b3cf92def1bb64f445440dccfb358978cf3bd23
saharsh0610 <saharsh.gupta06@gmail.com>
2021-10-19 17:34:26 +0530

    commit on 'master'

---
+++ web development/python/snackdown1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int T, A, B, C;
+int main(){
+    cin>>T;
+    for(int i = 0; i < T; i++){
+
+        cin>>A>>B>>C;
+        if (A == 7 || B== 7 ||C == 7  )
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+        
+    }
+
+}
